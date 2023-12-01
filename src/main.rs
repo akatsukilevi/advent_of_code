faddf4c54bbc7cb0cc0bc009f33b71e2fa952702
Akatsuki Levi <akatsukilevi@duck.com>
2023-12-01 20:34:01 -0300

    Hashmaps FTW

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -6,7 +6,6 @@ use std::path::Path;
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
-
     if args.len() == 1 {
         println!("Specify a input file");
         return;
@@ -22,29 +21,94 @@ fn main() {
         panic!("Failed to read file {}", &filename);
     };
 
-    let re = Regex::new(r"[^0-9]").unwrap();
+    let mut matcher: HashMap<&str, i32> = HashMap::new();
     let mut count = 0;
 
+    matcher.insert("one", 1);
+    matcher.insert("two", 2);
+    matcher.insert("three", 3);
+    matcher.insert("four", 4);
+    matcher.insert("five", 5);
+    matcher.insert("six", 6);
+    matcher.insert("seven", 7);
+    matcher.insert("eight", 8);
+    matcher.insert("nine", 9);
+
     while let Some(Ok(line)) = &lines.next() {
-        // * Remove all non number characters
-        let clean = re.replace_all(line, "");
-        if clean == "" {
+        if line == "" {
             continue; // * Ignore empty lines
         }
 
-        let first = clean.chars().next().unwrap();
-        let last = clean.chars().nth_back(0).unwrap();
-        let number_str = format!("{}{}", first, last);
+        // * Iterate through the entire line from left to right
+        let mut first: Option<i32> = None;
+        let mut last: Option<i32> = None;
 
+        let mut acc = String::from("");
+        let mut first_chars = line.chars();
+        while let Some(first_char) = first_chars.next() {
+            acc = acc + &first_char.to_string();
+
+            if let Some(first_num) = first_char.to_digit(10) {
+                first = Some(first_num.try_into().unwrap());
+                break;
+            }
+
+            for (k, v) in &matcher {
+                if acc.contains(k) {
+                    first = Some(v.clone());
+                    break;
+                }
+            }
+
+            if first.is_some() {
+                break;
+            }
+        }
+
+        acc = String::from("");
+        let mut last_chars = line.chars();
+        while let Some(last_char) = last_chars.next_back() {
+            acc = last_char.to_string() + &acc;
+
+            if let Some(last_num) = last_char.to_digit(10) {
+                last = Some(last_num.try_into().unwrap());
+                break;
+            }
+
+            for (k, v) in &matcher {
+                if acc.contains(k) {
+                    last = Some(v.clone());
+                    break;
+                }
+            }
+
+            if last.is_some() {
+                break;
+            }
+        }
+
+        let Some(first_num) = first else {
+            continue;
+        };
+
+        let Some(last_num) = last else {
+            continue;
+        };
+
+        let number_str = format!("{}{}", first_num, last_num);
         let Ok(number) = number_str.parse::<i32>() else {
             println!(
                 "Failed to parse line {}: Invalid number {}",
-                &clean, &number_str
+                &line, &number_str
             );
             continue;
         };
 
         count = count + number;
+        println!(
+            "Line {} - Number: {} - Counter: {}",
+            &line, &number_str, &count
+        );
     }
 
     println!("Result: {}", count);
